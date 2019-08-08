2d57c2d6ddeeb9e8f07d525e34d3fa50f0249cc7
ben-pr-p <ben.paul.ryan.packer@gmail.com>
2019-08-08 17:18:40 -0400

    enforce max concurrency on the prefetch level

--- src/lib/rabbit-runner.ts
+++ src/lib/rabbit-runner.ts
@@ -27,6 +27,7 @@ function defineSetupWorkerQueue(
       return; // todo - logging
     }
 
+    await channel.prefetch(MAX_CONCURRENCY);
     await channel.assertQueue(queueName, { durable: true });
     await channel.bindQueue(queueName, ASSEMBLE_EXCHANGE, queueName);
     log('Set up queue: %s', queueName);

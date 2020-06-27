da477d81c8727d0e0422afc58ef3dc08ab6a9216
Benjamin Chrobot <benjamin.chrobot@alum.mit.edu>
2020-06-27 15:21:27 -0400

    feat: switch to configurable winston logger

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,7 @@ import { migrate } from './lib/migrate';
 import { makePgFunctions } from './lib/pg-functions';
 import { createRunner } from './lib/rabbit-runner';
 import { Pool } from 'pg';
+import { Logger, createLogger, format, transports } from 'winston';
 
 const DEFAULT_POKE_INTERVAL = 10 * 1000;
 
@@ -13,6 +14,7 @@ type AssembleWorkerOptions = {
   taskList: TaskList;
   pokeInterval?: number;
   skipAutoMigrate?: boolean;
+  logger?: Logger;
 };
 
 export async function run(options: AssembleWorkerOptions) {
@@ -21,6 +23,13 @@ export async function run(options: AssembleWorkerOptions) {
   const pool = options.pgPool
     ? options.pgPool
     : new Pool({ connectionString: options.databaseConnectionString });
+
+  const logger = options.logger
+    ? options.logger
+    : createLogger({
+        format: format.combine(format.timestamp(), format.json()),
+        transports: [new transports.Console({ level: 'info' })]
+      });
 
   if (!skipAutoMigrate) {
     const client = await pool.connect();

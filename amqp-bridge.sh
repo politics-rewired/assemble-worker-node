00147f9f0bcd95611b5066f2eb8a31bfa78cb4b8
Benjamin Chrobot <benjamin.chrobot@alum.mit.edu>
2021-10-19 11:24:32 -0400

    ci: use docker run instead of create

--- amqp-bridge.sh
+++ amqp-bridge.sh
@@ -9,13 +9,15 @@ GITHUB_NETWORK=$(docker network ls | grep github | awk '{ print $2 }')
 ./dockerize -wait tcp://localhost:5432 -timeout 30s
 ./dockerize -wait tcp://localhost:5672 -timeout 30s
 
-docker create \
+docker run \
+  --detach \
+  --name  "amqp-bridge" \
   --network "bridge" \
   --network "$GITHUB_NETWORK" \
-  -e AMQP_URI="amqp://guest:guest@rabbitmq" \
-  -e POSTGRESQL_URI="postgres://postgres:postgres@postgres:5432/assemble_worker_test" \
-  -e DELIVERY_MODE="NON-PERSISTENT" \
-  -e BRIDGE_CHANNELS="assemble_worker:assemble_worker" \
+  --env AMQP_URI="amqp://guest:guest@rabbitmq" \
+  --env POSTGRESQL_URI="postgres://postgres:postgres@postgres:5432/assemble_worker_test" \
+  --env DELIVERY_MODE="NON-PERSISTENT" \
+  --env BRIDGE_CHANNELS="assemble_worker:assemble_worker" \
   us-east4-docker.pkg.dev/assemble-services/apps/pg-amqp-bridge:1.3.1
 
 sleep 1

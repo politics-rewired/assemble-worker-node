801fddea7b5d43fd5bff032706b15241cf748046
Benjamin Chrobot <benjamin.chrobot@alum.mit.edu>
2021-10-19 10:43:46 -0400

    ci: expose ports

--- amqp-bridge.sh
+++ amqp-bridge.sh
@@ -4,13 +4,13 @@
 
 GITHUB_NETWORK=$(docker network ls | grep github | awk '{ print $2 }')
 
-./dockerize -wait tcp://localhost:5432 -timeout 1m
-./dockerize -wait tcp://localhost:5672 -timeout 1m
+./dockerize -wait tcp://localhost:5432 -timeout 30s
+./dockerize -wait tcp://localhost:5672 -timeout 30s
 
 docker create \
-  --bridge "$GITHUB_NETWORK" \
-  -e AMQP_URI="amqp://guest:guest@127.0.0.1" \
-  -e POSTGRESQL_URI="postgres://postgres:postgres@127.0.0.1:5432/assemble_worker_test" \
+  --network "$GITHUB_NETWORK" \
+  -e AMQP_URI="amqp://guest:guest@rabbitmq" \
+  -e POSTGRESQL_URI="postgres://postgres:postgres@postgres:5432/assemble_worker_test" \
   -e DELIVERY_MODE="NON-PERSISTENT" \
   -e BRIDGE_CHANNELS="assemble_worker:assemble_worker" \
   gcr.io/assemble-services/pg-amqp-bridge-node:1.0.1

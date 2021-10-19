0b1a1a9c4270fe74c6493e60c803b7b5eb70e306
Benjamin Chrobot <benjamin.chrobot@alum.mit.edu>
2021-10-19 11:11:50 -0400

    ci: use gar image

--- amqp-bridge.sh
+++ amqp-bridge.sh
@@ -2,15 +2,20 @@
 
 # Run the pg-amqp-bridge image for CI tests
 
+docker network ls
+
 GITHUB_NETWORK=$(docker network ls | grep github | awk '{ print $2 }')
 
 ./dockerize -wait tcp://localhost:5432 -timeout 30s
 ./dockerize -wait tcp://localhost:5672 -timeout 30s
 
 docker create \
+  --network "bridge" \
   --network "$GITHUB_NETWORK" \
   -e AMQP_URI="amqp://guest:guest@rabbitmq" \
   -e POSTGRESQL_URI="postgres://postgres:postgres@postgres:5432/assemble_worker_test" \
   -e DELIVERY_MODE="NON-PERSISTENT" \
   -e BRIDGE_CHANNELS="assemble_worker:assemble_worker" \
-  gcr.io/assemble-services/pg-amqp-bridge-node:1.0.1
+  us-east4-docker.pkg.dev/assemble-services/apps/pg-amqp-bridge:1.3.1
+
+sleep 1
